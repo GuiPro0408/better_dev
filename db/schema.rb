535b189b555ebda2162b3fd8c9a92eb8e249d809
guillaume juste <guillaume.juste0408@gmail.com>
2022-11-03 19:29:01 +0400

    Add employment type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_173838) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_150831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_173838) do
     t.string "tech"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "employment_type"
   end
 
   create_table "reviews", force: :cascade do |t|
