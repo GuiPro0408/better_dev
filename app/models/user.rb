9989323002b6a1cc1713e9492e75d6082efbc8aa
guillaume juste <guillaume.juste0408@gmail.com>
2022-11-05 16:08:05 +0400

    Jobs controller done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :applications, dependent: :destroy
   has_many :jobs, through: :applications
+  has_many :jobs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
---
+++ db/migrate/20221105082749_add_user_to_jobs.rb
@@ -0,0 +1,5 @@
+class AddUserToJobs < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :jobs, :user, null: false, foreign_key: true
+  end
+end
