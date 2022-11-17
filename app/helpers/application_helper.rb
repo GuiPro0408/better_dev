0802d0c785a24b156a5a859fd44b6d5adadfd68c
Gaurav Chadee <hiji762@yahoo.com>
2022-11-17 21:04:13 +0400

    Design of homepage - OK

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def addclass(link)
+		if current_page?(link)
+			"active"
+		else
+			""
+		end
+	end
+	def addhoverclass(link)
+		if current_page?(link)
+			"actif"
+		else
+			""
+		end
+	end
 end
