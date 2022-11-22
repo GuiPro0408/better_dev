bc664a81ae44ee27cc9a2e1e6638f53fe226f3e5
guillaume juste <guillaume.juste0408@gmail.com>
2022-11-22 20:55:37 +0400

    remove new module

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,6 @@ const path    = require("path")
 const webpack = require("webpack")
 
 module.exports = {
-  module: {
-    loaders: [{
-      test: /\.jsx$|\.js$/,
-      loaders: ['babel-loader'],
-      include: path.resolve(__dirname, '../src')
-    },
-    {
-      test: /\.css$/,
-      loader: 'style-loader!css',
-    }, {
-      test: /\.woff2?$/,
-      loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      include: path.resolve(__dirname, '../assets')
-    },
-    {
-      test: /\.(eot|jpeg|jpg|png|svg|ttf|webp)$/,
-      loader: 'file-loader',
-      include: path.resolve(__dirname, '../assets')
-    },
-    {
-        test: /\.scss$/,
-        loader: 'style!css!sass?outputStyle=expanded',
-    },
-    {
-        test: /\.(png|jpg|svg)$/, loader: 'url-loader?limit=8192'
-    },
-    ]
-  },
   mode: "production",
   devtool: "source-map",
   entry: {
