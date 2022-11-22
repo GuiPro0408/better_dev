a9a59737bdf0fdf220e0a8b30b7217e1a35592b0
guillaume juste <guillaume.juste0408@gmail.com>
2022-11-22 20:07:37 +0400

    user redesign done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update,
                                       keys: [:nickname, :developer, :address, :mobile_phone, :website_url, :linkedin_url, :experience_year, :education_level, :bio, :image_url, :type_of_developers])
   end
+
+  def default_url_options
+    { host: ENV["https://www.better-dev-918.me"] || "localhost:3000" }
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,3 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 //= require turbolinks
-import "trix"
-import "@rails/actiontext"
